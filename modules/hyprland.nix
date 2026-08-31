9fb2cfb72bd6323770a0f3ba954f9aae0c09439d
kulbil <martin.danielsen@astralsaga.com>
2026-08-31 18:00:23 +0200

    added xdg portal for hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -2,4 +2,11 @@
 {
   programs.hyprland.enable = true;
   services.udisks2.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+  };
 }
