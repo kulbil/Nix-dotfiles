8ae119e399cc27154277de71e0c9a1ad91e7ae2d
kulbil <martin.danielsen@astralsaga.com>
2026-08-31 18:33:05 +0200

    changed

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -2,10 +2,9 @@
 {
   programs.hyprland.enable = true;
   services.udisks2.enable = true;
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
+
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
 
   xdg.portal = {
     enable = true;
